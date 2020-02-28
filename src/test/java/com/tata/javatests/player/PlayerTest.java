184fad28e696500e8f4fd5a10417bc7d884801e2
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-02-28 01:54:03 -0500

    10. simulando una clase con mockito

--- src/test/java/com/tata/javatests/player/PlayerTest.java
+++ src/test/java/com/tata/javatests/player/PlayerTest.java
@@ -1,20 +1,23 @@
 package com.tata.javatests.player;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
     @Test
     public void loose() {
-        Dice dice = new Dice(6);
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
         Player player = new Player(dice, 3);
         assertFalse(player.play());
     }
 
     @Test
     public void win() {
-        Dice dice = new Dice(6);
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(5);
         Player player = new Player(dice, 3);
         assertTrue(player.play());
     }
