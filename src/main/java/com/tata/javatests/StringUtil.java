8c0024fad8eb2efeda2c8a7bb4e04282aab8a799
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-02-28 00:55:33 -0500

    2. creando test para metodo repeat

--- src/main/java/com/tata/javatests/StringUtil.java
+++ src/main/java/com/tata/javatests/StringUtil.java
@@ -1,7 +1,7 @@
 package com.tata.javatests;
 
 public class StringUtil {
-    public String repeat(String text, int times) {
+    public static String repeat(String text, int times) {
         String result = "";
         for (int i = 0; i < times; i++) {
             result += text;
