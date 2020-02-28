c997afc0d693ba448426f014f3ae8a067b9540e7
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-02-28 01:33:27 -0500

    6. buenas practicas, probando distintos casos

--- src/test/java/com/tata/javatests/StringUtilTest.java
+++ src/test/java/com/tata/javatests/StringUtilTest.java
@@ -1,13 +1,26 @@
 package com.tata.javatests;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class StringUtilTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void repeat_string_once() {
+        String result = StringUtil.repeat("hola", 1);
+        assertEquals("hola", result);
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
         String result = StringUtil.repeat("hola", 3);
         assertEquals("holaholahola", result);
+    }
 
-        String result2 = StringUtil.repeat("hola", 1);
-        assertEquals("hola", result2);
+    @Test
+    public void repeat_string_zero_times() {
+        String result = StringUtil.repeat("hola", 0);
+        assertEquals("", result);
     }
 }
