e2bacb51436b3f7c15831c6232e7b007a97daf4a
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-02-28 01:30:48 -0500

    5. usando la librerio JUnit

--- src/test/java/com/tata/javatests/StringUtilTest.java
+++ src/test/java/com/tata/javatests/StringUtilTest.java
@@ -1,5 +1,7 @@
 package com.tata.javatests;
 
+import static org.junit.Assert.assertEquals;
+
 public class StringUtilTest {
     public static void main(String[] args) {
         String result = StringUtil.repeat("hola", 3);
@@ -8,12 +10,4 @@ public class StringUtilTest {
         String result2 = StringUtil.repeat("hola", 1);
         assertEquals("hola", result2);
     }
-
-    private static void assertEquals(String expected, String reality) {
-        if (!reality.equals(expected)) {
-            throw new RuntimeException("ERROR");
-        }
-    }
-
-
 }
