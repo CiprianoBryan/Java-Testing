a70a315efb7667a8623b2dfb63d2ad7d5a16c36a
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-02-28 00:58:02 -0500

    3. usando throw new para lanzar un error

--- src/test/java/com/tata/javatests/StringUtilTest.java
+++ src/test/java/com/tata/javatests/StringUtilTest.java
@@ -4,12 +4,12 @@ public class StringUtilTest {
     public static void main(String[] args) {
         String result = StringUtil.repeat("hola", 3);
         if (!result.equals("holaholahola")) {
-            System.out.println("ERROR");
+            throw new RuntimeException("ERROR");
         }
 
         String result2 = StringUtil.repeat("hola", 1);
         if (!result2.equals("hola")) {
-            System.out.println("ERROR");
+            throw new RuntimeException("ERROR");
         }
     }
 }
