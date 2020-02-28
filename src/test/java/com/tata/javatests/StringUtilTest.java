e9b651ab5c7fecb04adc4d4d8924e6c1fac9e3dd
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-02-28 01:05:21 -0500

    4. modularizando

--- src/test/java/com/tata/javatests/StringUtilTest.java
+++ src/test/java/com/tata/javatests/StringUtilTest.java
@@ -3,13 +3,17 @@ package com.tata.javatests;
 public class StringUtilTest {
     public static void main(String[] args) {
         String result = StringUtil.repeat("hola", 3);
-        if (!result.equals("holaholahola")) {
-            throw new RuntimeException("ERROR");
-        }
+        assertEquals("holaholahola", result);
 
         String result2 = StringUtil.repeat("hola", 1);
-        if (!result2.equals("hola")) {
+        assertEquals("hola", result2);
+    }
+
+    private static void assertEquals(String expected, String reality) {
+        if (!reality.equals(expected)) {
             throw new RuntimeException("ERROR");
         }
     }
+
+
 }
