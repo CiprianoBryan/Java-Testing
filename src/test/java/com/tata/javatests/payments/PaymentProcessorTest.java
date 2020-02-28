93cc74023ec0979e0b7c44bb7a4836f63ee5c29c
Bryan Cipriano <ciprianot.bryan@gmail.com>
2020-02-28 03:07:49 -0500

    12. setup en test, partes de un test

--- src/test/java/com/tata/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/tata/javatests/payments/PaymentProcessorTest.java
@@ -1,17 +1,24 @@
 package com.tata.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    PaymentGateway paymentGateway;
+    PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentStatus.OK));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
         assertTrue(paymentProcessor.makePayment(1000));
     }
 
